28a6dc3856e77504cd6aee3a5ff38a6bf3163f31
Sebsprograms <sebastianbenad72@gmail.com>
2023-06-04 11:12:33 +0200

    Add stopwatch functionality

--- lib/stop_watch.dart
+++ lib/stop_watch.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StopWatch extends StatefulWidget {
@@ -8,32 +10,70 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
-  String time = "00:00:00:00";
+  String time = "00 : 00 : 00";
+  Timer? timer;
+  Stopwatch stopwatch = Stopwatch();
 
   List<Widget> recordedTimes = [];
 
-  void start() {}
+  void updateTime(int milliseconds) {
+    setState(() {
+      int totalSeconds = milliseconds ~/ 1000;
+      int seconds = totalSeconds % 60;
+      int minutes = totalSeconds ~/ 60 % 60;
+      int hours = totalSeconds ~/ 3600;
 
-  void stop() {}
+      time =
+          '${addZero(hours.toString())} : ${addZero(minutes.toString())} : ${addZero(seconds.toString())}';
+    });
+  }
+
+  String addZero(String input) {
+    while (input.length < 2) {
+      input = '0$input';
+    }
+    return input;
+  }
+
+  void start() {
+    if (timer != null && timer!.isActive) return;
+    stopwatch.start();
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      updateTime(stopwatch.elapsedMilliseconds);
+    });
+  }
+
+  void stop() {
+    stopwatch.stop();
+    timer?.cancel();
+    timer = null;
+  }
 
   void record() {
     setState(() {
-      recordedTimes.add(Container(
-        padding: const EdgeInsets.only(
-          top: 10,
-        ),
-        child: Text(
-          time,
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.white,
-          ),
-        ),
-      ));
+      recordedTimes.insert(
+          0,
+          Container(
+            padding: const EdgeInsets.only(
+              top: 10,
+            ),
+            child: Text(
+              time,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ));
     });
   }
 
   void reset() {
+    stopwatch.reset();
+    updateTime(stopwatch.elapsedMilliseconds);
+  }
+
+  void clear() {
     setState(() {
       recordedTimes = [];
     });
@@ -91,7 +131,7 @@ class _StopWatchState extends State<StopWatch> {
             ElevatedButton(
               onPressed: reset,
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+                backgroundColor: Colors.orange,
               ),
               child: const Text("Reset",
                   style: TextStyle(
@@ -103,20 +143,39 @@ class _StopWatchState extends State<StopWatch> {
         const SizedBox(
           height: 20,
         ),
-        ElevatedButton(
-          onPressed: record,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.yellow,
-          ),
-          child: const Text("Record",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              )),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: record,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow,
+              ),
+              child: const Text("Record",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  )),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            ElevatedButton(
+              onPressed: clear,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text("Clear",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ],
         ),
         SizedBox(
           height: 90,
           child: SingleChildScrollView(
+            // reverse: true,
             scrollDirection: Axis.vertical,
             child: Column(
               children: recordedTimes,
